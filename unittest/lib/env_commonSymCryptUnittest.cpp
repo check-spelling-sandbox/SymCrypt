b0cab1010dc55e1a94686f93926405722c11b519
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-13 20:43:06 -0400

    spelling: transparent
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- unittest/lib/env_commonSymCryptUnittest.cpp
+++ unittest/lib/env_commonSymCryptUnittest.cpp
@@ -102,7 +102,7 @@ typedef SYMCRYPT_ALIGN_STRUCT _SYMCRYPT_ENV_XMM_SAVE_DATA_REGS {
     //
     // The alignment on x86 is only 4, so we can't align the __m128i fields properly.
     // We add some padding and let the assembler code adjust the alignment of the actual data.
-    // This is all transperant to the C code
+    // This is all transparent to the C code
     //
     __m128i xmm[8];         // 8 for the XMM registers.
     SYMCRYPT_MAGIC_FIELD
